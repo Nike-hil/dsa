9d474ba201b73ae7817fff2a3cb1f06e373be1ad
Nikhil <nikhil.paulite@gmail.com>
2025-07-02 18:21:16 +0530

    Initial commit - DSA problems and solutions

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class TwoSome {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int r = target - nums[i];
+
+            if (map.containsKey(r) && map.get(r) != i) {
+                return new int[] { i, map.get(r) };
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
